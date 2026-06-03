a63be840c6584a633d63d07bdee1e74416f54397
The Who <dashtikh@rambler.ru>
2026-06-03 18:24:05 +0300

    change viriable

--- src/test/java/PDSAPI/specs/ConstantValues.java
+++ src/test/java/PDSAPI/specs/ConstantValues.java
@@ -5,7 +5,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ConstantValues {
     private static final Dotenv dotenv = Dotenv.configure().load();
 
-    public static final String BASE_URL = dotenv.get("BASE_URL") != null ? dotenv.get("BASE_URL") : "https://test.soglasie-vita.ru/PO.Insurance/services/partner";
+    public static final String BASE_URL = dotenv.get("BASE_URL") != null ? dotenv.get("BASE_URL") : "";
     public static final String AUTH_ENDPOINT = "/v1/auth/login";
     public static final String DICT_ENDPOINT = "/v1/getDictionaries";
     public static final String CALC_ENDPOINT = "/v1/calculate";
