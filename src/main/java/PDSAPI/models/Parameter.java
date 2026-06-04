09684c5d05b51d5d4c5142682c7ad90348a76e32
The Who <dashtikh@rambler.ru>
2026-06-04 15:28:53 +0300

    fix parameter model

--- src/main/java/PDSAPI/models/Parameter.java
+++ src/main/java/PDSAPI/models/Parameter.java
@@ -1,5 +1,6 @@
 package PDSAPI.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,11 +10,16 @@ public class Parameter {
 
     private String code;
     private String name;
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     private String stringValue;
     private String type;
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     private Integer intValue;
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     private double decimalValue;
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     private Boolean boolValue;
+
 
     public Parameter (String code, String name, String stringValue, String type) {
         this.code = code;
