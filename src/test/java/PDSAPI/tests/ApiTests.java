2cb7981c8a186bfde6594c17fd161050bd639dcf
The Who <dashtikh@rambler.ru>
2026-06-04 13:06:09 +0300

    start add Allure report

--- src/test/java/PDSAPI/tests/ApiTests.java
+++ src/test/java/PDSAPI/tests/ApiTests.java
@@ -1,19 +1,26 @@
 package PDSAPI.tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.restassured.AllureRestAssured;
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 
+@Epic("Тестовый сайт")
+@Feature("Проверка роботоспособности")
 public class ApiTests {
 
     @Test
+    @Description("Проверка открытия сайта")
     public void testGetUser() {
         // Базовый URL, общий для всех запросов
         given().baseUri("https://reqres.in")
+                .filter(new AllureRestAssured())
                 .when()
                 .get("/docs")
                 .then()
                 .statusCode(200); // Проверяем статус код
-//                .body("data.id", equalTo(2)) // Проверяем значение поля в JSON
-//                .log().all(); // Логируем детали ответа
     }
 }
