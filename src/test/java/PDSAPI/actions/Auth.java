8b75d114a42fe7b9ebca106652464c24902b679e
The Who <dashtikh@rambler.ru>
2026-06-05 16:00:42 +0300

    add asserts to actions

--- src/test/java/PDSAPI/actions/Auth.java
+++ src/test/java/PDSAPI/actions/Auth.java
@@ -2,9 +2,12 @@ package PDSAPI.actions;
 
 import PDSAPI.models.AuthRequest;
 import PDSAPI.models.AuthResponse;
+import PDSAPI.models.Error;
 import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class Auth {
     public static String loginUser(String BaseUrl, String Endpoint, String Login, String Password) {
@@ -19,9 +22,29 @@ public class Auth {
                 .post(Endpoint).
                 then()
                 .statusCode(200)
+//                .log().all()
                 .extract()
                 .as(AuthResponse.class);
+
+        try {
+            assertNotNull(response.getSessionToken());
+        } catch (AssertionError e) {
+            throw new AssertionError(response.getMessage());
+        }
         
         return response.getSessionToken();
     }
 }
+
+
+//try {
+//assertNull(response.getWarnings());
+//        } catch (AssertionError e) {
+//StringBuilder errorMessages = new StringBuilder();
+//            for (
+//Error error : response.getWarnings().getErrors()) {
+//        errorMessages.append(error.getDetailMessage()).append("\n");
+//            }
+//
+//                    throw new AssertionError(errorMessages.toString().trim());
+//        }
---
+++ src/test/java/PDSAPI/actions/Import.java
@@ -0,0 +1,40 @@
+package PDSAPI.actions;
+
+import PDSAPI.models.*;
+import PDSAPI.models.Error;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class Import {
+    public static String ImportPolicy (String BaseUrl, String Endpoint, String sessionToken, PolicyImport policy) {
+        ImportRequest user = ImportRequest.builder()
+                .policy(policy).build();
+        ImportResponse response = given()
+                .baseUri(BaseUrl)
+                .header("sessionToken", sessionToken)
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(user).
+                when()
+                .post(Endpoint).
+                then()
+                .statusCode(200)
+                .log().all()
+                .extract()
+                .as(ImportResponse.class);
+
+        try {
+            assertNull(response.getWarnings());
+        } catch (AssertionError e) {
+            StringBuilder errorMessages = new StringBuilder();
+            for (Error error : response.getWarnings().getErrors()) {
+                errorMessages.append(error.getDetailMessage()).append("\n");
+            }
+
+            throw new AssertionError(errorMessages.toString().trim());
+        }
+        return response.getPolicy().getCalcID();
+    }
+}
